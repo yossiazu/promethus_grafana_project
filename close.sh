b106f7b8d9e948ed971817be39a744a47ccc8323
Yossi Azulay <121163420+yossiazu@users.noreply.github.com>
2023-07-10 14:26:45 +0300

    Update close.sh

--- close.sh
+++ close.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-kubectl delete -f configmap_cluster.yaml -n monitoring
-kubectl delete -f alert.yaml -n monitoring
+kubectl delete -f config/configmap_cluster.yaml -n monitoring
+kubectl delete -f config/alert.yaml -n monitoring
 
 helm uninstall prometheus prometheus-community/kube-prometheus-stack -n monitoring
 helm uninstall my-redis bitnami/redis -n redis
