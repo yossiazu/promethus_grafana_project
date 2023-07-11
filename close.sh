91afc8ac2d72e92d07a262525a9cbd5aa2c3bebb
Yossi Azulay <121163420+yossiazu@users.noreply.github.com>
2023-07-11 18:23:06 +0300

    Update close.sh

--- close.sh
+++ close.sh
@@ -3,7 +3,7 @@
 kubectl delete -f config/configmap_cluster.yaml -n monitoring
 kubectl delete -f config/alert.yaml -n monitoring
 
-helm uninstall my-grafana-loki bitnami/grafana-loki -n loki 
+helm uninstall my-grafana-loki bitnami/grafana-loki -n monitoring
 helm uninstall prometheus prometheus-community/kube-prometheus-stack -n monitoring
 helm uninstall my-redis bitnami/redis -n redis
 helm uninstall my-rabbitmq bitnami/rabbitmq -n rabbitmq
