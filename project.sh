4579cb13a50fd32c16c1e6437c53e81dd3c37012
Yossi Azulay <121163420+yossiazu@users.noreply.github.com>
2023-07-11 17:38:30 +0300

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NAMESPACE_NAMES=("redis" "rabbitmq" "monitoring")
+NAMESPACE_NAMES=("redis" "rabbitmq" "monitoring" "loki")
 
 for NAMESPACE_NAME in "${NAMESPACE_NAMES[@]}"; do
   NAMESPACE_EXISTS=$(kubectl get namespace "$NAMESPACE_NAME" --output=name 2>/dev/null)
@@ -16,8 +16,8 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-helm install my-grafana-loki bitnami/grafana-loki --version 2.10.0 -n monitoring --values values_loki.yaml
 helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring --values config/values_promethus.yaml
+helm install my-grafana-loki bitnami/grafana-loki --version 2.10.0 -n loki --values values_loki.yaml
 helm install my-redis bitnami/redis --version 17.11.6 -n redis --values config/values_redis.yaml
 helm install my-rabbitmq bitnami/rabbitmq --version 12.0.4 -n rabbitmq --values config/values_rabbitmq.yaml
 
